7142e71e3921873d53ec6fb27097b448c537ed54
SAHANAGN <sahanagnxworkz@gmail.com>
2022-09-12 12:43:27 +0530

    byte file

---
+++ Byte.java
@@ -0,0 +1,23 @@
+class Byte{
+	public static void main(String[] args){
+		
+		byte bag=9;
+		byte brand=3;
+		byte name=2;
+		byte size=4;
+		
+		System.out.println(bag);
+		System.out.println(brand);
+            bag =19;
+		brand=11;
+		name=12;
+		size=14;
+		
+		
+		System.out.println(bag);
+		System.out.println(brand);
+		
+		
+
+		}
+}
